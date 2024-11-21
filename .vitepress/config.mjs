1ef1a0138fee4269689e881b4221a6724c3cda1a
shy <2481109896@qq.com>
2024-11-21 14:26:33 +0800

    master

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  // base: '/personal-blog/',
   title: "Shy's personal blog",
   description: "A VitePress Site",
   themeConfig: {
