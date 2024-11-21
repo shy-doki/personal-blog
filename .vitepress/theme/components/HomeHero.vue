68a97226477b48a7484d714810c8e96279cf7adc
shy <2481109896@qq.com>
2024-11-21 19:11:51 +0800

    master

--- .vitepress/theme/components/HomeHero.vue
+++ .vitepress/theme/components/HomeHero.vue
@@ -15,13 +15,13 @@
 import { ref } from 'vue';
 const images = [
     '/personal-blog/icon/吃瓜.svg',
-    // '/personal-blog/icon/加油.svg',
-    // '/personal-blog/icon/打call.svg',
-    // '/personal-blog/icon/酷.svg',
-    // '/personal-blog/icon/撇嘴.svg',
-    // '/personal-blog/icon/色.svg',
-    // '/personal-blog/icon/右哼哼.svg',
-    // '/personal-blog/icon/晕.svg',
+    '/personal-blog/icon/加油.svg',
+    '/personal-blog/icon/打call.svg',
+    '/personal-blog/icon/酷.svg',
+    '/personal-blog/icon/撇嘴.svg',
+    '/personal-blog/icon/色.svg',
+    '/personal-blog/icon/右哼哼.svg',
+    '/personal-blog/icon/晕.svg',
 ]; 
 const randomImage = ref(images[Math.floor(Math.random() * images.length)]);
 </script>
